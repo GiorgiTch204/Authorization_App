b8f742d8e5d04e7f62a341c08ebe7b864568c7f4
22100523-oss <22100523@ibsu.edu.ge>
2026-06-20 20:26:43 +0400

    Changed writing login/route.js files BFF structure.

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -2,31 +2,40 @@ import { cookies } from "next/headers";
 
 export async function POST(request: Request){
     try{
-        await request.json();
+        const {username, password} = await request.json();
 
-        const response = fetch("https://dummyjson.com/auth/login", {
+        const response = await fetch("https://dummyjson.com/auth/login", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
-                username: "emilys",
-                password: "emilyspass",
+                username: username,
+                password: password,
                 expiresInMins: 60
             }),
         });
 
-        if(response.ok){
-            const data = await response.json();
-            return new Response(JSON.stringify(data), {status: 200});
-        } else {
+        if(!response.ok){
             const errorData = await response.json();
             const errorMessage = errorData.message || "Login failed";
                 return new Response(errorMessage, {status: response.status});
             }
+
+            const data = await response.json();
+                
+        const cookieStore = await cookies();
+
+        cookieStore.set("session", data.accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            path: "/",
+            maxAge: 60*60
+        });
+
+        return Response.json({username: data.username, email:data.email,})
+
         }
-    } catch(reason){
-        const message = reason instanceof Error ? reason.message : "Unexpected error";
-
-        return new Response(message, {status: 500});
+    catch(reason){
+        return new Response("Invalid request", {status: 400});
     }
-
 }
